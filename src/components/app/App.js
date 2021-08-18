664e21491402c1038aa5077547a6847e753d5e13
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-08-18 12:26:49 +0300

    added header in Header.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
+import Heder from '../header/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world!
-        </p>
+        <Heder/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+function Heder() {
+    return (<header class="header"> 
+           
+    <nav class="menu">
+        <ul>
+            <li><a href="/">Домашняя</a></li>
+            <li><a href="/" >О нас</a></li>
+            <li><a href="/">Ресурсы</a></li>
+            <li><a href="/">Контакты</a></li>
+        </ul>
+    </nav>        
+</header>);
+}
+
+export default Heder;
