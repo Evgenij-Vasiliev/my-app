59c81129ddc6ccf1fe7644bf3a41e1551597eeeb
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-08-19 17:24:45 +0300

    added footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,25 +2,15 @@
 import './App.css';
 import Heder from '.././header/Header'
 import Main from '.././main/Main'
+import Footer from '.././footer/Footer'
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Heder/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div className="App">      
       <Heder/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,16 @@
+import './Footer.css'
+
 function Footer() {
-    return <div>Footer</div>
+    return <div>
+        <footer className="footer">
+        <div className="text"><a href="/">Текст1</a></div>        
+        <div className="text"><a href="/">Текст2</a></div>        
+        <div className="text"><a href="/">Текст3</a></div>        
+        <div className="text"><a href="/">Текст4</a></div>        
+        <div className="text"><a href="/">Текст5</a></div>        
+        <div className="text"><a href="/">Текст6</a></div>        
+    </footer> 
+    </div>
 }
 
 export default Footer;
