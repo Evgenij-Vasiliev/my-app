5ad29d27eb0bc5725762cf1051a7059612b81311
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-08-20 11:50:27 +0300

    added sidebar and content

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,21 +1,12 @@
 import './Main.css'
+import Sidebar from './sidebar/Sidebar'
+import Content from './content/Content'
 
 function Main() {
     return <div>
         <main className="main">
-        <aside className="aside">
-            <ul>
-                <li><a href="/">Мой профиль</a></li>
-                <li><a href="/" >Мои покупки</a></li>
-                <li><a href="/">Мои адреса</a></li>
-                <li><a href="./contact.html">Мои контакты</a></li>
-            </ul>
-        </aside>
-        <div className="content">
-            <h2 className="heading">Контент</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet optio magnam esse, quo ad eveniet odio perferendis sapiente doloribus distinctio molestias maiores facilis eaque aliquid neque accusantium atque harum accusamus error consequuntur blanditiis itaque quaerat ex praesentium. Officia voluptates eius vitae ipsum unde voluptatum assumenda, quisquam amet aliquam corrupti maxime odit praesentium ipsa expedita neque rerum itaque dolorem nam beatae, incidunt ab, facere eaque. Nihil, architecto reprehenderit quae dolore quaerat cupiditate accusamus. Atque eveniet, natus nulla, consequuntur velit unde beatae nesciunt reiciendis odit assumenda aliquam ipsam facere officia. Laborum vel velit neque temporibus, facilis reiciendis accusantium ad, unde quisquam aliquid, autem amet in sed cum dolorem totam incidunt voluptatum odit vitae nostrum. Ex nostrum inventore ea aliquid, vitae, incidunt doloremque laborum voluptatum natus maiores sapiente dolore qui minima quis ad tempora quod amet, fugiat praesentium sit ipsum? Iusto, itaque deserunt quidem quasi voluptates velit! Hic doloremque voluptas asperiores praesentium, placeat laborum veniam explicabo quasi dignissimos cupiditate, delectus magni eum quod ex id accusantium? Maxime, officia! Suscipit odit nobis voluptate eum mollitia nam similique corrupti, id modi veritatis tempore, earum tempora obcaecati iste, totam numquam. Vero inventore nostrum sapiente asperiores optio voluptates iusto maxime doloribus adipisci quos nemo eligendi, laboriosam omnis?</p>
-        </div>
-        
+        <Sidebar/>              
+        <Content/>              
     </main>
     </div>        
 }
---
+++ src/components/main/sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import './Sidebar.css'
+
+
+function Sidebar() {
+    return <div>        
+        <aside className="aside">
+            <ul>
+                <li><a href="/">Мой профиль</a></li>
+                <li><a href="/" >Мои покупки</a></li>
+                <li><a href="/">Мои адреса</a></li>
+                <li><a href="./contact.html">Мои контакты</a></li>
+            </ul>
+        </aside>        
+    </div>        
+}
+
+export default Sidebar;
+
