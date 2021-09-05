863e2456416f468b7caea6b8a5d95f17a17c2c08
Evgenij-Vasiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-09-05 19:12:33 +0300

    added module.css

--- src/components/main/profile/Profile.js
+++ src/components/main/profile/Profile.js
@@ -1,5 +1,8 @@
+import styles from  './Profile.module.css'
+
+
 function Profile() {
-    return <div><h2>МОЙ ПРОФИЛЬ</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error commodi aliquid numquam nobis! Officia, accusantium numquam harum nisi nesciunt ipsa vero maxime? Aut voluptatum laboriosam odit laudantium nulla distinctio velit.</p></div>
+    return <div className={styles.myProfile}><h2>МОЙ ПРОФИЛЬ</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error commodi aliquid numquam nobis! Officia, accusantium numquam harum nisi nesciunt ipsa vero maxime? Aut voluptatum laboriosam odit laudantium nulla distinctio velit.</p></div>
 }
 
 export default Profile;
