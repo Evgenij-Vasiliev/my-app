5a67fd4516941b567048e54dadd158f93729acce
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-09-13 23:41:50 +0300

    added component Users.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,6 +5,7 @@ import Profile from './profile/Profile';
 import Cart from './cart/Cart';
 import Address from './address/Address';
 import Contact from './contact/Contact';
+import Users from './users/Users';
 import { Route, Switch, useRouteMatch } from 'react-router';
 
 function Main() {
@@ -16,7 +17,8 @@ function Main() {
             <Route path={match.path + '/profile'}><Profile/></Route>
             <Route path={match.path + '/cart'}><Cart/></Route>
             <Route path={match.path + '/address'}><Address/></Route>
-            <Route path={match.path + '/contact'}><Contact/></Route>            
+            <Route path={match.path + '/contact'}><Contact/></Route>
+            <Route path={match.path + '/users'}><Users/></Route>            
             <Route path={match.path + '/'}><Content/></Route>            
         </Switch>                      
     </main>
---
+++ src/components/main/users/Users.js
@@ -0,0 +1,12 @@
+import styles from  './Users.module.css'
+
+function Users() {
+    return <>
+    <div className={styles.usersAll}><h2>Все пользователи</h2></div>
+    <div>
+        const [users, setUsers] = useState([]);
+    </div>
+    </>
+}
+
+export default Users;
