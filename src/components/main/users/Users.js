166ff0d8d0fc890e4cd59254ce64f7f0cc2d3fb7
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-09-17 11:20:08 +0300

    added delete user

--- src/components/main/users/Users.js
+++ src/components/main/users/Users.js
@@ -4,27 +4,50 @@ import styles from  './Users.module.css';
 function Users() {
 
     const [users, setUsers] = useState([]);
+    const [isLoading, setLoading] = useState(false);    
 
-    if(!users.length) {
-        fetch('http://localhost:3500/users')
+    function getUsers() {
+
+        setLoading(true);
+
+        return fetch('http://localhost:3500/users')
         .then(result => {
             return result.json();
         }).then(data => {
             setUsers(data);
-        });        
+            setLoading(false);
+        });   
+    }
+
+    function removeUser(userId) {       
+        fetch(`http://localhost:3500/users/${userId}`, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            return getUsers();           
+        });
+    };
+        // }).then(() => {
+        //     let newUsers = users.filter(user => user.id !== userId) ;
+        //     setUsers(newUsers);
+        // })
+    
+
+    if(!users.length && !isLoading) {
+        getUsers();     
     }
 
     let htmlUsers = users.map((user) => {
         return <div key={user.id} className={styles.userRow}>
             <div className={styles.userInfo}>
             {user.id}:  {user.email} {user.password}                
-            </div>         
+            </div>
 
-            <button className={styles.btn}>X</button>                
-        </div>
-    });    
+            <button className={styles.btn} onClick={() => removeUser(user.id)}>X</button>                
+            </div>
+            });    
 
-    return <div className={styles.usersAll}><h2>Все пользователи</h2>{htmlUsers}</div>;   
+        return <div className={styles.usersAll}><h2>Все пользователи</h2>{htmlUsers}</div>;   
 }
 
 export default Users;
