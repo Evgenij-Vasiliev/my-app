6a35b4687b959bd5b16505d3de6f64a3bd747e7a
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-10-29 11:35:43 +0300

    the end

--- src/components/timer2/Timer2.js
+++ src/components/timer2/Timer2.js
@@ -2,43 +2,51 @@ import { useState, useEffect } from "react";
 // import styles from './Timer2.module.css';
 
 function Timer2() {
-  const [seconds, setSeconds] = useState(0);
-  const [timerActive, setTimerActive] = useState(false);
-  useEffect(() => {
+    console.log("Timer2 init")
+    const [seconds, setSeconds] = useState(0);
+    const [timerActive, setTimerActive] = useState(false);
+    useEffect(() => {
+        console.log("effect start");
     let timerId;
     if (timerActive) {
-      timerId = setInterval(() => {
-        setSeconds(seconds + 1);
-        console.log(seconds);
-      }, 1000);
-    } else if (timerId) {
-      clearInterval(timerId);
+        timerId = setInterval(() => {
+        setSeconds(seconds + 1); 
+        console.log("setSeconds")
+        }, 1000);
+        } else if (timerId) {
+        clearInterval(timerId);
+        }
+
+        return () => {
+        clearInterval(timerId);
+        console.log("end")
+        };
+    }, [timerActive, seconds]);
+
+    function timerStart() {
+        setTimerActive(true);
     }
 
-    return () => {
-      clearInterval(timerId);
-    };
-  }, [timerActive]);
+    function timerStop() {
+        setTimerActive(false);
+    }
 
-  function timerStart() {
-    setTimerActive(true);
-  }
+    function timerReset() {
+        timerStop();
+        setSeconds(0);
+    }
 
-  function timerStop() {
-    setTimerActive(false);
-  }
+    let minutes = `${Math.floor(seconds / 60)}`.padStart(2, '0');
+    let secondsTime = seconds - minutes * 60;
+    secondsTime = secondsTime < 10 ? `0${secondsTime}`: secondsTime;
 
-  function timerReset() {
-    setSeconds(0);
-  }
-
-  return (
-    <div>
-      <span>{seconds}</span>
-      <button onClick={timerStart}>Start</button>
-      <button onClick={timerStop}>Stop</button>
-      <button onClick={timerReset}>Reset</button>
-    </div>
-  );
+    return (
+        <div>
+        <span>{minutes}:{secondsTime}</span>
+        <button onClick={timerStart}>Start</button>
+        <button onClick={timerStop}>Stop</button>
+        <button onClick={timerReset}>Reset</button>
+        </div>
+    );
 }
 export default Timer2;
