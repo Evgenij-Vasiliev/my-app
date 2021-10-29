37d958e4fbd9fa8c83efc3afd2f119cbc35eca54
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-10-29 10:39:46 +0300

    added useEffect

--- src/components/timer2/Timer2.js
+++ src/components/timer2/Timer2.js
@@ -1,20 +1,44 @@
-import { useState } from 'react';
-import styles from './Timer2.module.css';
+import { useState, useEffect } from "react";
+// import styles from './Timer2.module.css';
 
 function Timer2() {
-    const [seconds, useSeconds] = useState(0);
-    function timerStart() {
-        setInterval(() => {
-            useSeconds(seconds + 1);
-        }, 1000)                        
+  const [seconds, setSeconds] = useState(0);
+  const [timerActive, setTimerActive] = useState(false);
+  useEffect(() => {
+    let timerId;
+    if (timerActive) {
+      timerId = setInterval(() => {
+        setSeconds(seconds + 1);
+        console.log(seconds);
+      }, 1000);
+    } else if (timerId) {
+      clearInterval(timerId);
     }
 
-    return <div>
-        <span>{ seconds }</span>
-        <button onClick={ timerStart }>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
-        </div>
-    
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [timerActive]);
+
+  function timerStart() {
+    setTimerActive(true);
+  }
+
+  function timerStop() {
+    setTimerActive(false);
+  }
+
+  function timerReset() {
+    setSeconds(0);
+  }
+
+  return (
+    <div>
+      <span>{seconds}</span>
+      <button onClick={timerStart}>Start</button>
+      <button onClick={timerStop}>Stop</button>
+      <button onClick={timerReset}>Reset</button>
+    </div>
+  );
 }
 export default Timer2;
